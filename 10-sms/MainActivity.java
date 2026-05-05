1b34d2cc04c00b8c8e136e98a95d32b5922381e2
Mohammed Rayan A <mohammedrayan9064@gmail.com>
2026-05-05 18:34:15 +0530

    first

--- 10-sms/MainActivity.java
+++ 10-sms/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.smsdemo2;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
@@ -10,50 +11,36 @@ import android.widget.Toast;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-public class MainActivity extends
-AppCompatActivity {
-Button btn1;
-@SuppressLint("MissingInflatedId")
-@Override
-protected void onCreate(Bundle
-savedInstanceState){
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main);
-//request permission for sms
-ActivityCompat.requestPermissions(this,new
-String[]{Manifest.permission.SEND_SMS},
-PackageManager.PERMISSION_GRANTED);
-//create objects of views
-btn1 = (Button)
-findViewById(R.id.btnSendSMS);
-et1 =
-(EditText)findViewById(R.id.editText1);
-et2 = (EditText)
-findViewById(R.id.editText2);
-//on click listener
-btn1.setOnClickListener(new
-View.OnClickListener() {
-@Override
-public void onClick(View v) {
-    String number =
-et1.getText().toString();//fetching values of
-phone no. and message
-String
-sms=et2.getText().toString();
-try{
-SmsManager smsmanager =
-SmsManager.getDefault();//create object of
-smsmanager
-smsmanager.sendTextMessage(number,null,sms,null,nu
-ll);
-Toast.makeText(getApplicationContext(),"Message
-sent success",Toast.LENGTH_SHORT).show();
-}
-catch(Exception e)
-{
-e.printStackTrace();
-}
-}
-});
-}
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    EditText et1, et2;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // request permission for sms
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+        // create objects of views
+        btn1 = findViewById(R.id.btnSendSMS);
+        et1 = findViewById(R.id.editText1);
+        et2 = findViewById(R.id.editText2);
+        // on click listener
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = et1.getText().toString(); // fetching values of phone no. and message
+                String sms = et2.getText().toString();
+                try {
+                    SmsManager smsmanager = SmsManager.getDefault(); // create object of smsmanager
+                    smsmanager.sendTextMessage(number, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "Message sent success", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
--- 7-Menu/MainActivity.java
+++ 7-Menu/MainActivity.java
@@ -1,44 +1,40 @@
 package com.example.optionmenu;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-public class MainActivity extends AppCompatActivity {
- @Override
- protected void onCreate(Bundle
-savedInstanceState) {
- super.onCreate(savedInstanceState);
- setContentView(R.layout.activity_main);
- }
- @Override
- public boolean onCreateOptionsMenu(Menu menu)
-{
 
-getMenuInflater().inflate(R.menu.opt_menu,menu);
- return true;
- }
- public boolean onOptionsItemSelected(@NonNull
-MenuItem item)
- {
- int item_id=item.getItemId();
- if (item_id==R.id.option1) {
- Toast.makeText(this, "Opening new
-file", Toast.LENGTH_SHORT).show();
- return true;
- }
- if(item_id==R.id.option2) {
- Toast.makeText(this, "Saving file",
-Toast.LENGTH_SHORT).show();
-return true;
- }
- if (item_id==R.id.option3) {
- Toast.makeText(this, "Searching file",
-Toast.LENGTH_SHORT).show();
- return true;
- }
- return
-super.onOptionsItemSelected(item);
- }
- }
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.opt_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		int item_id = item.getItemId();
+		if (item_id == R.id.option1) {
+			Toast.makeText(this, "Opening new file", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		if (item_id == R.id.option2) {
+			Toast.makeText(this, "Saving file", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		if (item_id == R.id.option3) {
+			Toast.makeText(this, "Searching file", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
